5c61025a98846a7c0e7eaf902434f2038f755837
William Alvarez <walvarez1@my.bcit.ca>
2023-02-28 00:57:41 -0800

    feat: lint:fix script set up

--- README.md
+++ README.md
@@ -2,6 +2,12 @@ so that i dont have to move my wrists and fingers as much :)
 
 to be used only for mongo or other db with unstructured data and fast queries
 
+## todo
+
+controller, middleware, models, routes, util/lib templates
+
+test environment
+
 ## Scripts
 
 local preview
@@ -25,4 +31,5 @@ yarn lint:fix
 ## Other
 
 create a .env.local locally & add stuff from .env.template or w/e is needed
+
 update deps for every project started
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
 const port = 4000;
 
-app.get("/", (req: Request, res: Response) => res.send("hello"));
+app.get('/', (req: Request, res: Response) => res.send('hello'));
 
 app.listen(process.env.PORT || port, () =>
-   console.log(`Server started on http://localhost:${port}/`)
+  console.log(`Server started on http://localhost:${port}/`)
 );
